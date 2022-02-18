57dd34527a3c2db8721c780d4d95be2146302c68
vintenj <joe.vinten@vodafone.com>
2022-02-18 15:47:49 +0000

    create pipeline

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
 	// TODO implement
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify("Hello from Joe!"),
+		body: JSON.stringify("Hello from Joe's MacBook!"),
 	}
 	return response
 }
